e3b9450a7e4140fd783dd8f0a6c61ece74c76e4a
Mauve-negi <116134417+Mauve-negi@users.noreply.github.com>
2023-03-10 13:38:51 +0000

    [UpDate]ルーティングおよびトップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'homes/top'
+  # get 'books/top'
+  root to: 'homes#top'
+  get '/books' => 'books#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :homes
+  resources :books
+  
 end
